b898bbd0a52ff4faeb8813198aee65f3f120bc02
CJ Castillo <caytejohntims@gmail.com>
2026-02-13 08:28:35 +0800

    Add race to points and message if user quits

--- src/Main.groovy
+++ src/Main.groovy
@@ -1,4 +1,4 @@
-print"""
+print """
     _______
 ---'   ____)
       (_____)
@@ -7,7 +7,7 @@ print"""
 ---.__(___)
 """
 
-print"""
+print """
      _______
 ---'    ____)____
            ______)
@@ -16,7 +16,7 @@ print"""
 ---.__________)
 """
 
-print"""
+print """
     _______
 ---'   ____)____
           ______)
@@ -27,21 +27,31 @@ print"""
 
 int computerPoints = 0
 int userPoints = 0
+int raceToFinalPoints = 5
 
 def choices = ['rock', 'paper', 'scissors']
 
 while (true) {
-    print "Choose one - Rock, Paper, Scissors (type 'q' to quit): "
+    print "Choose one - Rock, Paper or Scissors (type 'q' to quit): "
     def userChoice = System.in.newReader().readLine().toLowerCase()
 
-    // When user quits, it will display the final score
+    // When user quit, it will display the final score
     if (userChoice == 'q') {
-        println 'Game ended'
-        println "Final Score -> You: ${userPoints} | Computer: ${computerPoints}"
+        println 'Game ended. You decided to quit.'
+        println "Final Score -> You: ${userPoints} | Computer: ${computerPoints} \n"
+
+        // If user quit message and who win the game
+        if (userPoints > computerPoints) {
+            print 'Congratulations! You won the game.'
+        } else if (userPoints == computerPoints) {
+            print "It's a tie. What a great game!"
+        } else {
+            print 'Better luck next time!'
+        }
         break
     }
 
-    // When the user's choice did not choose rock paper scissors
+    // When the user did not choose rock paper scissors
     if (!choices.contains(userChoice)) {
         println 'Invalid choice! Choose rock, paper or scissors'
         continue
@@ -49,10 +59,11 @@ while (true) {
 
     // Computer will randomly choose in list
     def computerChoice = choices[new Random().nextInt(choices.size())].toLowerCase()
-    println "Computer chose ${computerChoice}"
+    println "Computer choose ${computerChoice}"
 
-    // Determine who will win
-    if ((userChoice == 'rock' && computerChoice == 'scissors') || (userChoice == 'paper' && computerChoice == 'rock') ||
+    // Determine who will win the round
+    if ((userChoice == 'rock' && computerChoice == 'scissors') ||
+            (userChoice == 'paper' && computerChoice == 'rock') ||
             (userChoice == 'scissors' && computerChoice == 'paper')
     ) {
         println 'You win!'
@@ -64,4 +75,13 @@ while (true) {
         computerPoints++
     }
     println "\n Current Score -> You: ${userPoints} | Computer: ${computerPoints}\n"
+
+    // Race to final points (5 points)
+    if (userPoints == raceToFinalPoints) {
+        print "You reach ${raceToFinalPoints} points first. You win!"
+        break
+    } else if (computerPoints == raceToFinalPoints) {
+        print "Computer reach ${raceToFinalPoints} points first. Computer win!"
+        break
+    }
 }
